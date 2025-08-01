905496ece5305f741d9d4650d6d7a00269f32969
Mahmoud Hussien <m.hus.dev@gmail.com>
2025-08-01 23:23:30 +0300

    pokemon page control list

--- src/modules/super-admin/pokemon/components/pokemon-card.tsx
+++ src/modules/super-admin/pokemon/components/pokemon-card.tsx
@@ -4,25 +4,30 @@ import type { IPokemon } from '../interfaces/pokemon.interface';
 
 import { formatToThreeDigits } from '../utils/pokemon.utils';
 
+import { Card, CardContent } from '@/shared/components/ui/card';
+
 interface IProps {
   data: IPokemon;
 }
 
 const PokemonCard = ({ data }: IProps) => {
   return (
-    <Link to={`${data.id}`} className='bg-muted p-1.25rem rounded-md text-center'>
-      <div className='mb-1.25rem flex items-center justify-center'>
-        <img
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`}
-          alt={data.name}
-          width={100}
-          height={100}
-          loading='lazy'
-          className='object-contain'
-        />
-      </div>
-      <h3 className='text-lg font-bold capitalize'>{data.name}</h3>
-      <p className='text-muted-foreground'>{formatToThreeDigits(data.id)}</p>
+    <Link to={`${data.id}`} className=' '>
+      <Card className='bg-muted transition-all duration-300 hover:scale-105 hover:shadow-xl'>
+        <CardContent className='p-4 text-center'>
+          <div className='relative mb-3'>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`}
+              alt={data.name}
+              className='mx-auto h-24 w-24 object-contain'
+              loading='lazy'
+            />
+          </div>
+
+          <h3 className='text-lg font-bold capitalize'>{data.name}</h3>
+          <p className='text-muted-foreground'>#{formatToThreeDigits(data.id)}</p>
+        </CardContent>
+      </Card>
     </Link>
   );
 };

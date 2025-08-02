bfa44bbe5c5cd13f9aae9c81189a6c92b58da8a3
Mahmoud Hussien <m.hus.dev@gmail.com>
2025-08-02 18:51:13 +0300

    not found layout

---
+++ src/layouts/error.layout.tsx
@@ -0,0 +1,5 @@
+const ErrorLayout = () => {
+  return <div>ErrorLayout</div>;
+};
+
+export default ErrorLayout;
--- src/layouts/not-fount.layout.tsx
+++ src/layouts/not-fount.layout.tsx
@@ -1,5 +1,26 @@
+import Container from '@/shared/components/container';
+
+import { ButtonLink } from '@/shared/components/ui/button-link';
+
 const NotFoundLayout = () => {
-  return <div>NotFoundLayout</div>;
+  return (
+    <Container>
+      <div className='flex h-screen flex-col items-center justify-center'>
+        <p className='text-primary mb-4 text-sm font-semibold uppercase md:text-base'>
+          That’s a 404
+        </p>
+        <h1 className='mb-2 text-center text-2xl font-bold text-gray-800 md:text-3xl'>
+          Page not found
+        </h1>
+
+        <p className='mb-12 max-w-screen-md text-center text-gray-500 md:text-lg'>
+          The page you’re looking for doesn’t exist.
+        </p>
+
+        <ButtonLink to='/'>Go Home</ButtonLink>
+      </div>
+    </Container>
+  );
 };
 
 export default NotFoundLayout;
